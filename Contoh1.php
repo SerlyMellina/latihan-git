bb567d207ae4ffe93e5ba4ba90a54f0a9c5f1c0a
SerlyMellina <serlymellina9@gmail.com>
2022-10-19 21:59:15 +0700

    commit

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Serly Mellina
+ Saya tinggal di daerah Karawang
+ saya suka menggambar";
+	}
+}
